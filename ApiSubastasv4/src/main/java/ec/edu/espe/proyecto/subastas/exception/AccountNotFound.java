47f1cd5c6a426a977f8072714bf01f2a0be339b5
ohnini <elchangoluisa2@espe.edu.ec>
2025-03-04 03:01:04 -0500

    .

--- ApiSubastasv4/src/main/java/ec/edu/espe/proyecto/subastas/exception/AccountNotFound.java
+++
@@ -1,7 +0,0 @@
-package ec.edu.espe.proyecto.subastas.exception;
-
-public class AccountNotFound extends Exception{
-    public AccountNotFound(String message) {
-        super(message);
-    }
-}
--- ApiSubastasv4/src/main/java/ec/edu/espe/proyecto/subastas/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package ec.edu.espe.proyecto.subastas.exception;
-
-public class InvalidCredentialsException extends Exception{
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
